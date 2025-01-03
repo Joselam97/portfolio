a214deddf362f9c85a9f8e25a3b88710f0f0495a
Jose Alvarez <jlam97@hotmail.es>
2025-01-03 17:05:33 -0600

    Design and functions of portfolio are finished

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import HomeComponent from "./components/HomeComponent/HomeComponent";
 import { Hind } from "next/font/google";
 import MySlider from "./components/SliderCard/MySlider";
 import AboutMe from "./components/AboutMe/AboutMe";
+import CallToAction from "./components/CallToAction/CallToAction";
 
 const hind = Hind({
   subsets: ["latin"],
@@ -38,6 +39,7 @@ export default function Home() {
         </div>
       </div>
       <AboutMe/>
+      <CallToAction/>
 
     </main>
   );
